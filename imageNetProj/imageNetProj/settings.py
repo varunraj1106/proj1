2366e83334827e4364ba6f56a8f1eda9653f098f
varunraj1106 <86312464+varunraj1106@users.noreply.github.com>
2022-01-20 14:10:45 +0530

    Update settings.py

--- imageNetProj/imageNetProj/settings.py
+++ imageNetProj/imageNetProj/settings.py
@@ -136,3 +136,9 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
+
+
+import os
+import django_heroku
+
+django_heroku.settings(locals())
